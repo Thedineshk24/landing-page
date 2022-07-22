7761e30e08c0fcd55d88a45a08d2978483ae22d3
Dinesh Katariya <thedineshk24@gmail.com>
2022-07-22 11:21:42 +0530

    issue fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,12 @@ import ErrorBoundary from './ErrorBoundry/MyErrorBoundry';
 import HomePage from './HomePage';
 
 const Home: NextPage = () => {
+  // if css is loading then show loading screen
+  if (typeof window !== 'undefined') {
+    if (window.__NEXT_DATA__.props.pageProps.isLoading) {
+      return <div>Loading...</div>
+    }
+  }
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
   </Head>
